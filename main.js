dc316204c9db2ea1063f9988f355b50223b69fe0
Shivam Singh <shivam152002@gmail.com>
2021-11-02 01:34:37 +0530

    feat: append dom elements to popup and integrate API data

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+/*
+ *  Creator: @1502shivam-singh
+ *  <shivam152002@gmail.com>
+ *
+ *  ✔ Implement popup trigger when highlighing text
+ *  ✔ Debounce selection of text, to eliminate multiple wasteful API calls when highlighting
+ *  ✔ Establish system for closing popup when clicking outside popup
+ *  ✔ Establish system for closing popup when clicking outside popup
+ *----------------------------------------------------------------------------------
+ *  Response data structure -
+ *  {
+ *      meanings (Array of objects)
+ *          [
+ *              {
+ *              definitions (Array of objects)
+ *                  [{antonyms (Array), definition (string), example (string), synonyms (Array)}]
+ *              , partOfSpeech (string)
+ *              }
+ *          ],
+ *      origin (string),
+ *      phonetic (string)
+ *      phonetics (Array of objects)
+ *          [
+ *              {
+ *                  audio (string)
+ *                  text (string)
+ *              }
+ *          ]
+ *  } 
+ *
+ */
 const start = async () => {
     let [tab] = await chrome.tabs.query({
         active: true,
@@ -20,38 +51,18 @@ const start = async () => {
 
 start();
 
-/*
- *
- *  ✔ Implement popup trigger when highlighing text
- *  ✔ Debounce selection of text, to eliminate multiple wasteful API calls when highlighting
- *  ✔ Establish system for closing popup when clicking outside popup
- *  ✔ Establish system for closing popup when clicking outside popup
- *  
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 function seekHighlight() {
     let string;
 
     const getSelectedText = (e) => {
-        console.log({
-            string
-        });
         if (string.length > 1) {
             const popUp = document.createElement("div");
             popUp.className = "popUpContainer";
             popUp.tabIndex = 0;
 
             const popUpDim = {
-                width: 500,
-                height: 200,
+                width: 400,
+                height: 240,
             }
 
             const closePopUpEvent = (event) => {
@@ -64,18 +75,11 @@ function seekHighlight() {
                     y2: e.pageY + popUpDim.height,
                 }
 
-                console.log({
-                    xIn,
-                    yIn
-                });
                 if (!((event.pageX >= xIn.x1 && event.pageX <= xIn.x2) && (event.pageY >= yIn.y1 && event.pageY <= yIn.y2))) {
-                    console.log(event.pageX, event.pageY);
-
                     gsap.to(popUp, {
                         opacity: 0,
                         duration: 0.3,
                         onComplete: () => {
-                            console.log("Closing popUp");
                             window.removeEventListener('click', closePopUpEvent);
                             popUp.remove();
                         }
@@ -98,7 +102,7 @@ function seekHighlight() {
                     opacity: 0,
                     padding: "10px",
                     filter: "drop-shadow(0 4px 10px rgb(0,0,0,.25))",
-                    
+                    overflowY: "scroll",
                 }
             )
 
@@ -108,7 +112,6 @@ function seekHighlight() {
             });
 
             document.body.appendChild(popUp);
-            console.log(e.screenX, e.screenY);
 
             fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${string}`)
                 .then(response => response.json())
@@ -116,11 +119,19 @@ function seekHighlight() {
                     const {
                         meanings,
                         origin,
+                        phonetic,
                         phonetics
                     } = data[0];
-                    console.log(meanings, origin, phonetics);
+                    console.log({
+                        meanings,
+                        origin,
+                        phonetic,
+                        phonetics
+                    });
 
-                    let fragment = document.createDocumentFragment();
+                    //--------------------------------------------------------
+
+                    let meaningFragment = document.createDocumentFragment();
 
                     let popUpbox = document.createElement('div');
                     let wordAndAudio = document.createElement('div');
@@ -128,63 +139,20 @@ function seekHighlight() {
                     let audioSvg = document.createElement('object');
                     let wordSpell = document.createElement('p');
                     let wordPhonetic = document.createElement('p');
-                    let type = document.createElement('div');
-                    let meaning = document.createElement('p');
-                    let sentence = document.createElement('li');
-                    let similar = document.createElement('p');
-                    let synonyms = document.createElement('p');
-
-                    const marginZero = {
-                        margin: 0
-                    };
-
-                    let styleMap = new Map([
-                        [popUpbox.style, {
-                            fontFamily: "Maison Neue",
-                        }],
-                        [wordAndAudio.style, {
-                            display: "flex",
-                            alignItems: "center",
-                        }],
-                        [audio.style, {
-                            padding: "5px 0 0 0",
-                        }],
-                        [audioSvg.style, {
-                            width: "40px",
-                        }],
-                        [wordSpell.style, {
-                            fontSize: "1.3rem",
-                            padding: "0 0 0 16px",
-                            margin: 0,
-                        }],
-                        [wordPhonetic.style, {
-                            fontFamily: "monospace",
-                            padding: "0 0 0 56px",
-                            fontSize: "1rem",
-                            margin: 0,
-                        }],
-                        [type.style, {
-                            margin: "10px 0",
-                        }],
-                        [sentence.style, {
-                            margin: "5px 0 10px 0",
-                        }],
-                        [meaning.style, marginZero],
-                        [similar.style, marginZero],
-                        [synonyms.style, marginZero],
-                    ]);
-
-                    styleMap.forEach((value, key)=>{
-                        Object.assign(key, value);
-                    });
 
                     audioSvg.type = "image/svg+xml";
                     audioSvg.data = chrome.runtime.getURL("assets/audio.svg");
                     audioSvg.className = "audio-svg";
                     audioSvg.textContent = "Speaker logo";
 
-                    audio.class = "audio";
-                    audio.appendChild(audioSvg);
+                    audio.className = "audio";
+
+                    let phoneticAudio = new Audio(phonetics[0].audio);
+                    audio.addEventListener('click', (e)=>{
+                        phoneticAudio.play();
+                    });
+                    
+                    audio.appendChild(audioSvg);                    
 
                     //------------------------------------
                     wordSpell.className = "word-spell";
@@ -196,51 +164,94 @@ function seekHighlight() {
                     //-------------------------------------
 
                     wordPhonetic.className = "word-phonetic";
-                    wordPhonetic.textContent = "/ˈɛpɪɡram/";
-                    //-------------------------------------
-
-                    type.className = "type";
-                    type.textContent = "noun";
-                    //-------------------------------------
-
-                    meaning.className = "meaning";
-                    meaning.textContent = "a pithy saying or remark expressing an idea in a clever and amusing way."
-                    //-------------------------------------
-
-                    sentence.className = "sentence";
-                    sentence.textContent = "\"a Wildean epigram\""
-                    //--------------------------------------
-
-                    similar.textContent = "similar";
-
-                    const syns = ["quip", "witticism", "gem", "play on words", "jest", "pun", "sally"]
-                    let synString = "";
-
-                    syns.forEach((it, pos) => {
-                        if (pos != 6) {
-                            synString += `${it}, `;
-                        } else {
-                            synString += `${it}`;
-                        }
-                    });
-
-                    synonyms.textContent = synString;
-                    //-------------------------------------
+                    wordPhonetic.textContent = `/${phonetic}/`;
 
                     popUpbox.className = "popup-box";
 
                     popUpbox.appendChild(wordAndAudio);
                     popUpbox.appendChild(wordPhonetic);
-                    popUpbox.appendChild(type);
-                    popUpbox.appendChild(meaning);
-                    popUpbox.appendChild(sentence);
-                    popUpbox.appendChild(similar);
-                    popUpbox.appendChild(synonyms);
 
-                    fragment.appendChild(popUpbox);
-                    popUp.appendChild(fragment);
+                    meanings.forEach((meaning, index) => {
+                        let type = document.createElement('div');
+                        type.className = "type";
+                        type.textContent = meaning.partOfSpeech;
+                        meaningFragment.appendChild(type);
 
 
+                        meaning.definitions.forEach((info, i) => {
+
+                            let subDescription = document.createElement('div');
+                            subDescription.className = "subDescription";
+
+                            let meaning = document.createElement('p');
+
+                            meaning.className = "meaning";
+                            meaning.textContent = `${i+1}.  ${info.definition}`;
+                            subDescription.appendChild(meaning);
+
+                            if (info.example != undefined) {
+                                let sentence = document.createElement('li');
+                                sentence.className = "sentence";
+                                sentence.textContent = `\"${info.example}\"`;
+                                subDescription.appendChild(sentence);
+                            }
+
+                            if (info.synonyms != undefined && info.synonyms.length !== 0) {
+                                let similar = document.createElement('span');
+                                similar.textContent = "similar: ";
+                                let synonyms = document.createElement('div');
+                                let lineBreak = document.createElement('br');
+                                similar.className = "similar";
+                                synonyms.className = "synonyms";
+                                let synString = "";
+
+                                for (let index = 0; index < info.synonyms.length; index++) {
+                                    const element = info.synonyms[index];
+                                    if (index === 6 || index === info.synonyms.length - 1) {
+                                        synString += `${element}`;
+                                        break;
+                                    } else {
+                                        synString += `${element}, `;
+                                    }
+                                }
+
+                                synonyms.textContent = synString;
+                                subDescription.appendChild(similar);
+                                subDescription.appendChild(synonyms);
+                                subDescription.appendChild(lineBreak);
+                            }
+
+                            if (info.antonyms != undefined && info.antonyms.length !== 0) {
+                                let opposites = document.createElement('span');
+                                opposites.textContent = "antonyms: ";
+                                let antonyms = document.createElement('div');
+
+                                opposites.className = "opposites";
+                                antonyms.className = "antonyms";
+
+                                let antString = "";
+
+                                for (let index = 0; index < info.antonyms.length; index++) {
+                                    const element = info.antonyms[index];
+                                    if (index === 6 || index === info.antonyms.length - 1) {
+                                        antString += `${element}`;
+                                        break;
+                                    } else {
+                                        antString += `${element}, `;
+                                    }
+                                }
+
+                                antonyms.textContent = antString;
+                                subDescription.appendChild(opposites);
+                                subDescription.appendChild(antonyms);
+                            }
+
+                            meaningFragment.appendChild(subDescription);
+                        });
+                    })
+
+                    popUpbox.appendChild(meaningFragment);
+                    popUp.appendChild(popUpbox);
                 });
         }
         document.removeEventListener('mouseup', getSelectedText);
